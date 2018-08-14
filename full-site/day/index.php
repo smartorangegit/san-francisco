6f8cc2e584a929f6f31448547335341309266941
Begas Anna <anneta.begas@gmail.com>
2018-08-14 11:46:01 +0300

    add page "day"

---
+++ full-site/day/index.php
@@ -0,0 +1,74 @@
+<? /*head*/ HeadAdd($html=['head'=>'Y']);	?>
+<link rel="stylesheet" href="/css/day.css">
+
+    <div class="main_page long_page clearfix">
+           <?  /*Menu*/ MenuAdd();  ?>
+
+                <div class="content content_day">
+                  <ul class="day_slider">
+                    <li class="day_item day_item__1" data-time="08:00" data-text="Прокинутися у просторій квартирі із видом на парк"></li>
+                    <li class="day_item day_item__2" data-time="09:00" data-text="Поснідати у улюбленій кав’ярні, що розташована у вашому будинку"></li>
+                    <li class="day_item day_item__3" data-time="10:00" data-text="Провести продуктивну зустріч із однодумцями у IT-Hub"></li>
+                    <li class="day_item day_item__4" data-time="12:00" data-text="Прогулятися парком біля будинку "></li>
+                    <li class="day_item day_item__5" data-time="13:00" data-text="Пообідати із друзями у ресторані на першому поверсі"></li>
+                    <li class="day_item day_item__6" data-time="14:00" data-text="За п’ятнадцять хвилин дістатися центру, щоб провести декілька зустрічей"></li>
+                    <li class="day_item day_item__7" data-time="16:00" data-text="Повернутися до San Francisco, щоб попрацювати у Creative Lounge"></li>
+                    <li class="day_item day_item__8" data-time="18:00" data-text="Відвідати супермаркет на першому поверсі будинку"></li>
+                    <li class="day_item day_item__9" data-time="19:00" data-text="Разом з коханою людиною приготувати смачну романтичну вечерю"></li>
+                    <li class="day_item day_item__10" data-time="21:00" data-text="Подивитися з друзями у домашньому кінотеатрі новий фільм, події якого відбуваються у чарівному місті Сан-Франциско"></li>
+                  </ul>
+
+                  <div class="day_figcaption">
+                    <div class="day_inner">
+                      <div class="content_subname">
+                        1 день у <br>SAN FRANCISCO Creative House
+                      </div>
+                      <div class="time_box">
+                        <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 47.001 47.001" height="18" viewBox="0 0 47 47" width="18" fill="#ffffff"><path d="m22.432 22.429c0 .591.479 1.067 1.068 1.067s1.068-.479 1.068-1.067c.001-1.25.445-2.465 1.251-3.421 1.793-2.126 3.137-4.431 3.988-6.851.115-.327.065-.689-.135-.972-.201-.283-.525-.451-.872-.451h-10.601c-.347 0-.672.168-.873.451-.2.283-.25.645-.135.972.853 2.42 2.195 4.725 3.988 6.851.807.956 1.252 2.172 1.253 3.421z"/><path d="m24.568 26.71c0-.59-.479-1.067-1.068-1.067s-1.068.479-1.068 1.067c-.001 1.542-.922 3.067-2.593 4.304-3.574 2.639-6.249 5.506-7.951 8.52-.187.332-.184.736.009 1.062.19.329.542.53.922.53h21.364c.379 0 .73-.201.922-.53.191-.326.194-.73.008-1.062-1.701-3.014-4.377-5.881-7.95-8.52-1.673-1.237-2.594-2.762-2.595-4.304z"/><path d="m42.192 42.729h-.639c-.734-8.313-5.602-14.695-9.386-19.228 3.785-4.532 8.651-10.915 9.388-19.228h.639c1.18 0 2.136-.957 2.136-2.137 0-1.18-.956-2.136-2.136-2.136h-37.387c-1.18 0-2.136.957-2.136 2.136 0 1.18.956 2.137 2.136 2.137h.639c.735 8.314 5.601 14.697 9.386 19.228-3.784 4.532-8.651 10.914-9.387 19.228h-.638c-1.18 0-2.136.955-2.136 2.135 0 1.181.956 2.138 2.136 2.138h2.671 32.044 2.672c1.18 0 2.136-.957 2.136-2.138 0-1.18-.957-2.135-2.138-2.135zm-32.464 0c.803-7.511 5.686-13.295 9.335-17.617l.195-.231c.672-.795.672-1.959 0-2.755l-.194-.23c-3.648-4.323-8.533-10.107-9.336-17.619h27.544c-.803 7.512-5.688 13.296-9.336 17.619l-.193.23c-.672.795-.672 1.959 0 2.755l.195.231c3.648 4.322 8.531 10.106 9.334 17.617z"/></svg>
+                        <span id="timeOfDay">08:00</span>
+                      </div>
+                      <div class="content_text" id="titleOfDay">
+                        Прокинутися у просторій квартирі із видом на парк
+                      </div>
+                      <ul class="day_pager">
+                        <li><a data-slide-index="0" data-time="08:00" href="#" class="day_time active"></a></li>
+                        <li><a data-slide-index="1" data-time="09:00" href="#" class="day_time "></a></li>
+                        <li><a data-slide-index="2" data-time="10:00" href="#" class="day_time "></a></li>
+                        <li><a data-slide-index="3" data-time="12:00" href="#" class="day_time "></a></li>
+                        <li><a data-slide-index="4" data-time="13:00" href="#" class="day_time "></a></li>
+                        <li><a data-slide-index="5" data-time="14:00" href="#" class="day_time "></a></li>
+                        <li><a data-slide-index="6" data-time="16:00" href="#" class="day_time "></a></li>
+                        <li><a data-slide-index="7" data-time="18:00" href="#" class="day_time "></a></li>
+                        <li><a data-slide-index="8" data-time="19:00"  href="#" class="day_time"></a></li>
+                        <li><a data-slide-index="9" data-time="21:00" href="#" class="day_time"></a></li>
+                      </ul>
+                    </div>
+                  </div>
+
+           </div>
+      </div>
+
+
+<? /*footer*/ FooterAdd($html=['head'=>'Y']);	?>
+<script type="text/javascript">
+var bxConfigDay = {
+  auto: true,
+  mode: 'fade',
+  autoHover: true,
+  captions: false,
+  controls: false,
+  pagerCustom: $('.day_pager'),
+  easing: 'ease-in-out',
+  onSliderLoad: function(currentIndex) {
+    $("#timeOfDay").html($('.day_item').eq(currentIndex).attr('data-time'));
+    $("#titleOfDay").html($('.day_item').eq(currentIndex).attr('data-text'));
+
+  },
+  onSlideBefore: function($slideElement, oldIndex, newIndex) {
+    $("#timeOfDay").html($slideElement.attr("data-time"));
+    $("#titleOfDay").html($slideElement.attr("data-text"));
+  }
+};
+var bxSlider = $('.day_slider').bxSlider(bxConfigDay);
+
+</script>
